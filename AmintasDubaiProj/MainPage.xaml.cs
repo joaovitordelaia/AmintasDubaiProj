7b3c714dd60b88b011d959d333598468d9327fea
João Vitor Delaia <joaovdelaia@gmail.com>
2023-10-24 17:22:43 -0300

    24.10.23 17:22

--- AmintasDubaiProj/MainPage.xaml.cs
+++ AmintasDubaiProj/MainPage.xaml.cs
@@ -5,5 +5,20 @@ public partial class MainPage : ContentPage
 	public MainPage()
 	{
 		InitializeComponent();
-	}
+    }
+
+    private void GenerateLuckNumber()
+    {
+        ContainerLuckNumbers.IsVisible = true;
+        NameAppTile.IsVisible = true;
+
+
+
+    }
+
+    private void ButtonLuckGeneretor_Clicked(object sender, EventArgs e)
+    {
+        GenerateLuckNumber();
+    }
+
 }
